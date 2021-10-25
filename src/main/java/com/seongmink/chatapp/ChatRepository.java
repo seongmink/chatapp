19e0a3daf776e8d39a1ea6b224c4c3c5655ca5a5
KSM <sminggo5@naver.com>
2021-10-26 00:17:10 +0900

    메시지 전송 및 조회 기능 추가

--- src/main/java/com/seongmink/chatapp/ChatRepository.java
+++ src/main/java/com/seongmink/chatapp/ChatRepository.java
@@ -2,10 +2,12 @@ package com.seongmink.chatapp;
 
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 import reactor.core.publisher.Flux;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
+    @Tailable
     @Query("{sender: ?0, receiver: ?1}")
     Flux<Chat> mFindBySender(String sender, String receiver);
 }
